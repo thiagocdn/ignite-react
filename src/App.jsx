dc6faedcec2f64200fde5cd6a4ac749fd4086b1e
Thiago Cedran Santos <thiagocdn@TCedran-Dell>
2022-10-17 19:56:09 -0300

    Started creating header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+import { Header } from "./components/header";
 import { Post } from "./Post";
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
-    <Post
-      author="Thiago Cedran"
-      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi iure eaque natus, assumenda reiciendis ea quasi rem laboriosam nesciunt saepe, dicta alias voluptas! Molestias rerum quasi iure corrupti quo cum?"
-    />
-    <Post
-      author="Thiago Cedran"
-      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi iure eaque natus, assumenda reiciendis ea quasi rem laboriosam nesciunt saepe, dicta alias voluptas! Molestias rerum quasi iure corrupti quo cum?"
-    />
-    <Post
-      author="Thiago Cedran"
-      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi iure eaque natus, assumenda reiciendis ea quasi rem laboriosam nesciunt saepe, dicta alias voluptas! Molestias rerum quasi iure corrupti quo cum?"
-    />
+      <Header />
+      <Post
+        author="Thiago Cedran"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi iure eaque natus, assumenda reiciendis ea quasi rem laboriosam nesciunt saepe, dicta alias voluptas! Molestias rerum quasi iure corrupti quo cum?"
+      />
+      <Post
+        author="Thiago Cedran"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi iure eaque natus, assumenda reiciendis ea quasi rem laboriosam nesciunt saepe, dicta alias voluptas! Molestias rerum quasi iure corrupti quo cum?"
+      />
+      <Post
+        author="Thiago Cedran"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi iure eaque natus, assumenda reiciendis ea quasi rem laboriosam nesciunt saepe, dicta alias voluptas! Molestias rerum quasi iure corrupti quo cum?"
+      />
     </div>
   )
 }
